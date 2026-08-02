c4b8c4d9be12ad2cb5d370fe754156f627fefa87
adityaBhatt02 <aditya.adi02bhatt@gmail.com>
2026-08-02 20:53:08 +0530

    added .proto file for grpc connection b/w auth and matchmaking service

---
+++ matchmaking-service/src/main/java/com/arenacore/matchmaking/controller/QueueController.java
@@ -0,0 +1,12 @@
+package com.arenacore.matchmaking.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/queue")
+@RequiredArgsConstructor
+public class QueueController {
+
+}
