e3ffd636488f255ed454f8cff9659757fb625417
adityaBhatt02 <aditya.adi02bhatt@gmail.com>
2026-08-08 21:43:36 +0530

    added proto file for lobby service and created lobby entity

---
+++ lobby-service/src/main/java/com/arenacore/lobby/entity/Lobby.java
@@ -0,0 +1,34 @@
+package com.arenacore.lobby.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "lobbies")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Lobby {
+
+    @Id
+    private String id = UUID.randomUUID().toString();
+
+    @Column(nullable = false)
+    private String status = "WAITING_FOR_READY";     // WAITING_FOR_READY, IN_PROGRESS, COMPLETED, CANCELLED
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String teamAPlayerIds;                   // comma-seperated player id's for now
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String teamBPlayerIds;
+
+    private Instant createdAt = Instant.now();
+}
